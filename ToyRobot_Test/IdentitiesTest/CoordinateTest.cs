78fa279ba87baebda708a7af31ba1b1b569b4b48
Carlos <carua97@gmail.com>
2023-06-08 16:05:13 +0200

    Add coordinate test

---
+++ ToyRobot_Test/IdentitiesTest/CoordinateTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToyRobotGame.src.Identities;
+
+namespace ToyRobot_Test.IdentitiesTest
+{
+    public class CoordinateTest
+    {
+        [Fact]
+        public void CoordinateSetRowAndColumn()
+        {
+            int testRow = 2;
+            int testColumn = 3;
+
+            Coordinate coordinate = new Coordinate(testRow, testColumn);
+
+            Assert.Equal(testRow, coordinate.Row);
+            Assert.Equal(testColumn, coordinate.Column);
+        }
+    }
+}
