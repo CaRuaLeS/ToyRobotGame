f6439fff97a19e70985c9e70d6e6a43fd773dbae
Carlos <carua97@gmail.com>
2023-06-08 20:03:50 +0200

    Create GameAction class and create PlaceRobot function

---
+++ ToyRobotGame/src/Action/GameAction.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToyRobotGame.src.Identities;
+using ToyRobotGame.src.Action;
+
+namespace ToyRobotGame.src.Action
+{
+    public class GameAction
+    {
+        RobotAction robotAction = new RobotAction();
+
+        public void PlaceRobot(int row, int column, Direction placeFacing)
+        {
+            Coordinate placeCoordinate = new(row, column);
+
+            if (robotAction.isValidCoordinate(placeCoordinate))
+            {                
+                Robot.Robot robot = new(placeCoordinate, placeFacing);
+
+                if (robot == null)
+                {
+                    robot = new Robot.Robot(placeCoordinate, placeFacing);
+                }
+                else
+                {
+                    robot.Position = placeCoordinate;
+                    robot.Facing = placeFacing;
+                }
+            }
+        }
+
+        public void PlaceWall()
+        {
+
+        }
+        public void Report (Robot.Robot robot)
+        {
+
+        }
+    }
+}
