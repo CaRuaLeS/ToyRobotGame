27fe9f11cd3b7366534c06728807c8b07e604325
Carlos <carua97@gmail.com>
2023-06-08 16:05:50 +0200

    Add direction test with each direction

---
+++ ToyRobot_Test/IdentitiesTest/DirectionTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToyRobotGame.src.Identities;
+
+namespace ToyRobot_Test.IdentitiesTest
+{
+    public class DirectionTest
+    {
+        [Fact]
+        public void DirectionNorthIs0()
+        {
+            Assert.Equal(0, (int)Direction.DirectionType.NORTH);
+        }
+        [Fact]
+        public void DirectionSouthIs1()
+        {
+            Assert.Equal(1, (int)Direction.DirectionType.SOUTH);
+        }
+        [Fact]
+        public void DirectionEastIs2()
+        {
+            Assert.Equal(2, (int)Direction.DirectionType.EAST);
+        }
+        [Fact]
+        public void DirectionWestIs3()
+        {
+            Assert.Equal(3, (int)Direction.DirectionType.WEST);
+
+        }
+
+    }
+}
