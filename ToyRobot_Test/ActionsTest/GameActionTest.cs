c66d5b73bf434e5ecf7dedfe5f0b3ca56dee3295
Carlos <carua97@gmail.com>
2023-06-09 11:47:42 +0200

    Add new testing combinations to PlaceRobot function

--- ToyRobot_Test/ActionsTest/GameActionTest.cs
+++ ToyRobot_Test/ActionsTest/GameActionTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using ToyRobotGame.src.Action;
@@ -12,21 +13,71 @@ namespace ToyRobot_Test.ActionsTest
 {
     public class GameActionTest
     {
+
         [Fact]
         public void PlaceRobot_IfNoRobotIsInTheField()
         {
             // Arrange
-
-            GameAction testGameAction = new GameAction();
+            
+            GameAction testGame = new GameAction();
             int testRow = 2;
             int testColumn = 3;
             var testDirection = Direction.NORTH;
 
             // Act
-            var testRobot = testGameAction.PlaceRobot(testRow, testColumn, testDirection);
+            testGame.PlaceRobot(testRow, testColumn, testDirection);
 
             // Assert
-            Assert.Equal(testRow, testRobot.Position.Row);
+            Assert.Equal(testRow, testGame.robotInstance.Position.Row);
+        }
+
+        [Fact]
+        public void PlaceRobot_IfPlaceChangesRobotPosition()
+        {
+            // Arrange
+            GameAction testGame = new GameAction();
+            Coordinate defaultTestCoordinate = new(1, 1);
+            var defaultTestDirection = Direction.NORTH;
+            testGame.robotInstance = new Robot(defaultTestCoordinate, defaultTestDirection);
+
+            // Assert --> check first default position
+            Assert.Equal(defaultTestCoordinate.Row, testGame.robotInstance.Position.Row);
+            Assert.Equal(defaultTestCoordinate.Column, testGame.robotInstance.Position.Column);
+            Assert.Equal(defaultTestDirection, testGame.robotInstance.Facing);
+
+            // Act
+            int testRow = 2;
+            int testColumn = 3;
+            testGame.PlaceRobot(testRow, testColumn, defaultTestDirection);
+
+            // Assert
+            Assert.Equal(testRow, testGame.robotInstance.Position.Row);
+            Assert.Equal(testColumn, testGame.robotInstance.Position.Column);
+            Assert.Equal(defaultTestDirection, testGame.robotInstance.Facing);
+        }
+        [Fact]
+        public void PlaceRobot_NoRobotInstantiatedAndChangeItsPosition()
+        {
+            // Arrange
+            GameAction testGame = new GameAction();
+            int testRow = 2;
+            int testColumn = 3;
+            var testDirection = Direction.NORTH;
+            var updatedTestDirection = Direction.SOUTH;
+            
+
+            // Act
+            testGame.PlaceRobot(testRow, testColumn, testDirection);
+            Assert.Equal(2, testGame.robotInstance.Position.Row);
+            Assert.Equal(3, testGame.robotInstance.Position.Column);
+            Assert.Equal(testDirection, testGame.robotInstance.Facing);
+
+            testGame.PlaceRobot(4, 4, updatedTestDirection);
+
+            // Assert
+            Assert.Equal(4, testGame.robotInstance.Position.Row);
+            Assert.Equal(4, testGame.robotInstance.Position.Column);
+            Assert.Equal(updatedTestDirection, testGame.robotInstance.Facing);
         }
     }
 }
