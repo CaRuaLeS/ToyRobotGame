c3ab7abc4762876cfc5ef54857ecfadc5aba0d0c
Carlos <carua97@gmail.com>
2023-06-09 17:57:28 +0200

    Add wall test, coordinates are correct

---
+++ ToyRobot_Test/Obstacles/WallTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToyRobotGame.src.Identities;
+using ToyRobotGame.src.Obstacles;
+using ToyRobotGame.src.Robot;
+
+namespace ToyRobot_Test.Obstacles
+{
+    public class WallTest
+    {
+        [Fact]
+        public void Wall_CreateWithCoordinates()
+        {
+            // Arrange
+            var testPosition = new Coordinate(0, 0);
+
+            // Act
+            var testWall = new Wall(testPosition);
+
+            // Assert
+            Assert.Equal(testPosition, testWall.Position);
+
+
+        }
+    }
+}
