34cfb0f18501b2192dc23cd367ee4dbacefe42bd
Carlos <carua97@gmail.com>
2023-06-09 17:48:57 +0200

    Add robot intern actions interface (move, right and left)

---
+++ ToyRobotGame/src/Interfaces/IRobotActions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyRobotGame.src.Interfaces
+{
+    public interface IRobotActions
+    {
+        void Move();
+        void LookLeft();
+        void LookRight();
+    }
+}
