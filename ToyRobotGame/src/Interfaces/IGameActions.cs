4859add1abdb399c0c272df82afbad55f3150fb6
Carlos <carua97@gmail.com>
2023-06-09 17:49:25 +0200

    Add game interface actions (placeRobot, placeWall and Report)

---
+++ ToyRobotGame/src/Interfaces/IGameActions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToyRobotGame.src.Identities;
+
+namespace ToyRobotGame.src.Interfaces
+{
+    public interface IGameActions
+    {
+        void PlaceRobot(int row, int column, Direction facing);
+        void PlaceWall(int row, int column);
+        void Report();
+    }
+}
