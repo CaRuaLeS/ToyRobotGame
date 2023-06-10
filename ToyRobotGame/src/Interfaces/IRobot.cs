5fa8b73c8ae4df265d24e4f358ae690ff201fa62
Carlos <carua97@gmail.com>
2023-06-10 11:16:49 +0200

    Add IRobot interface with Coordinate and Direction

---
+++ ToyRobotGame/src/Interfaces/IRobot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToyRobotGame.src.Identities;
+
+namespace ToyRobotGame.src.Interfaces
+{
+    public interface IRobot
+    {
+        Coordinate Position { get; set; }
+        Direction Facing { get; set; }
+    }
+}
