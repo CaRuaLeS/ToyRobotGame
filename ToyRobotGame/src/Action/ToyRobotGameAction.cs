da4dc76aa51fde1e64f3e761b640d0dbaefffc3b
Carlos <carua97@gmail.com>
2023-06-10 13:45:39 +0200

    Comment ToyRobotGameAction class and test, reorder function (DELETE IF UNUSED)

--- ToyRobotGame/src/Action/ToyRobotGameAction.cs
+++ ToyRobotGame/src/Action/ToyRobotGameAction.cs
@@ -10,49 +10,49 @@ using ToyRobotGame.src.Robot;
 
 namespace ToyRobotGame.src.Action
 {
-    public class ToyRobotGameAction: IGameActions
-    {
-        private const int XYBoardSize = 5;
-        private List<Wall> walls;
+    //public class ToyRobotGameAction: IGameActions
+    //{
+    //    private const int XYBoardSize = 5;
+    //    private List<Wall> walls;
 
-        public ToyRobotGameAction()
-        {
-            walls = new List<Wall>();
-        }
+    //    public ToyRobotGameAction()
+    //    {
+    //        walls = new List<Wall>();
+    //    }
 
-        public void PlaceWall(int row, int column, Robot.Robot robot) 
-        {
-            Coordinate wallCoordiante = new Coordinate(row, column);
+    //    public void PlaceWall(int row, int column, Robot.Robot robot) 
+    //    {
+    //        Coordinate wallCoordiante = new Coordinate(row, column);
 
-            if (isValidCoordinate(wallCoordiante))
-            {
-                Wall newWall = new(wallCoordiante);
-                if (!isOccupiedCoordinate(newWall.Position, robot))
-                {
-                    walls.Add(newWall);
-                }
-            }
-        }
+    //        if (isValidCoordinate(wallCoordiante))
+    //        {
+    //            Wall newWall = new(wallCoordiante);
+    //            if (!isOccupiedCoordinate(newWall.Position, robot))
+    //            {
+    //                walls.Add(newWall);
+    //            }
+    //        }
+    //    }
 
-        private bool isValidCoordinate(Coordinate coordinate)
-        {
-            if (coordinate.Row >= 1 && coordinate.Row <= XYBoardSize && coordinate.Column >= 1 && coordinate.Column <= XYBoardSize)
-            {
-                return true;
-            }
-            return false;
-        }
+    //    private bool isValidCoordinate(Coordinate coordinate)
+    //    {
+    //        if (coordinate.Row >= 1 && coordinate.Row <= XYBoardSize && coordinate.Column >= 1 && coordinate.Column <= XYBoardSize)
+    //        {
+    //            return true;
+    //        }
+    //        return false;
+    //    }
 
-        private bool isOccupiedCoordinate(Coordinate position, Robot.Robot robot)
-        {
+    //    private bool isOccupiedCoordinate(Coordinate position, Robot.Robot robot)
+    //    {
 
-            if (robot != null && robot.Position.Row == position.Row && robot.Position.Column == position.Column 
-                || walls.Exists(wall => wall.Position.Row == position.Row && wall.Position.Column == position.Column))
-            {
-                return true;
-            }
-            return false;
+    //        if (robot != null && robot.Position.Row == position.Row && robot.Position.Column == position.Column 
+    //            || walls.Exists(wall => wall.Position.Row == position.Row && wall.Position.Column == position.Column))
+    //        {
+    //            return true;
+    //        }
+    //        return false;
 
-        }
-    }
+    //    }
+    //}
 }
--- ToyRobot_Test/ActionsTest/RobotActionTest.cs
+++ ToyRobot_Test/ActionsTest/RobotActionTest.cs
@@ -10,129 +10,129 @@ using Xunit;
 
 namespace ToyRobot_Test.ActionsTest
 {
-    public class RobotActionTest
-    {
-        // Test wall with move left (NOT IMPLEMENTED)
-        [Fact]
-        public void Move_FacingNorth_ResultPositionToBe21()
-        {
-            // Arrange
-            Coordinate testCoordinate = new(1, 1);
-            Robot testRobot = new(testCoordinate, Direction.NORTH);
-            var action = new RobotAction();
+    //public class RobotActionTest
+    //{
+    //    // Test wall with move left (NOT IMPLEMENTED)
+    //    [Fact]
+    //    public void Move_FacingNorth_ResultPositionToBe21()
+    //    {
+    //        // Arrange
+    //        Coordinate testCoordinate = new(1, 1);
+    //        Robot testRobot = new(testCoordinate, Direction.NORTH);
+    //        var action = new RobotAction();
 
-            // Act
-            action.Move(testRobot);
+    //        // Act
+    //        action.Move(testRobot);
 
-            // Assert
-            Assert.NotNull(testRobot);
-            Assert.Equal(2, testRobot.Position.Row);
-            Assert.Equal(1, testRobot.Position.Column);
+    //        // Assert
+    //        Assert.NotNull(testRobot);
+    //        Assert.Equal(2, testRobot.Position.Row);
+    //        Assert.Equal(1, testRobot.Position.Column);
 
-        }
+    //    }
 
-        [Fact]
-        public void Move_FacingNorthPassTheEdge_ResultPositionToBe11()
-        {
-            // Arrange
-            Coordinate testCoordinate = new(4, 1);
-            Robot testRobot = new(testCoordinate, Direction.NORTH);
-            var action = new RobotAction();
+    //    [Fact]
+    //    public void Move_FacingNorthPassTheEdge_ResultPositionToBe11()
+    //    {
+    //        // Arrange
+    //        Coordinate testCoordinate = new(4, 1);
+    //        Robot testRobot = new(testCoordinate, Direction.NORTH);
+    //        var action = new RobotAction();
 
-            // Act
-            action.Move(testRobot);
+    //        // Act
+    //        action.Move(testRobot);
 
-            // Assert
-            Assert.Equal(1, testRobot.Position.Row);
-            Assert.Equal(1, testRobot.Position.Column);
+    //        // Assert
+    //        Assert.Equal(1, testRobot.Position.Row);
+    //        Assert.Equal(1, testRobot.Position.Column);
 
-        }
+    //    }
 
-        [Fact]
-        public void InvalidMove_TheresNoValidPosition_InRobotCoordinates()
-        {
-            // Arrange
-            Coordinate testErrorCoordinate = new(0, 0);
-            Robot testRobot = new(testErrorCoordinate, Direction.NORTH);
-            var action = new RobotAction();
+    //    [Fact]
+    //    public void InvalidMove_TheresNoValidPosition_InRobotCoordinates()
+    //    {
+    //        // Arrange
+    //        Coordinate testErrorCoordinate = new(0, 0);
+    //        Robot testRobot = new(testErrorCoordinate, Direction.NORTH);
+    //        var action = new RobotAction();
 
-            // Act
-            action.Move(testRobot);
+    //        // Act
+    //        action.Move(testRobot);
 
-            // Assert
-            Assert.Equal(0, testRobot.Position.Row);
-            Assert.Equal(0, testRobot.Position.Column);
+    //        // Assert
+    //        Assert.Equal(0, testRobot.Position.Row);
+    //        Assert.Equal(0, testRobot.Position.Column);
 
-        }
+    //    }
 
-        [Fact]
-        public void LookLeft_FinishLookingWest()
-        {
-            // Arrange
-            Coordinate testCoordinate = new(1, 1);
-            Robot testRobot = new(testCoordinate, Direction.NORTH);
-            var action = new RobotAction();
+    //    [Fact]
+    //    public void LookLeft_FinishLookingWest()
+    //    {
+    //        // Arrange
+    //        Coordinate testCoordinate = new(1, 1);
+    //        Robot testRobot = new(testCoordinate, Direction.NORTH);
+    //        var action = new RobotAction();
 
-            // Act
-            action.LookLeft(testRobot);
+    //        // Act
+    //        action.LookLeft(testRobot);
 
-            // Assert
-            Assert.Equal(Direction.WEST, testRobot.Facing);
+    //        // Assert
+    //        Assert.Equal(Direction.WEST, testRobot.Facing);
 
-        }
+    //    }
 
-        [Fact]
-        public void LookRight_FinishLookingEast()
-        {
-            // Arrange
-            Coordinate testCoordinate = new(1, 1);
-            Robot testRobot = new(testCoordinate, Direction.NORTH);
-            var action = new RobotAction();
+    //    [Fact]
+    //    public void LookRight_FinishLookingEast()
+    //    {
+    //        // Arrange
+    //        Coordinate testCoordinate = new(1, 1);
+    //        Robot testRobot = new(testCoordinate, Direction.NORTH);
+    //        var action = new RobotAction();
 
-            // Act
-            action.LookRight(testRobot);
+    //        // Act
+    //        action.LookRight(testRobot);
 
-            // Assert
-            Assert.Equal(Direction.EAST, testRobot.Facing);
+    //        // Assert
+    //        Assert.Equal(Direction.EAST, testRobot.Facing);
 
-        }
-        [Fact]
-        public void LookRight_FinishLookingNorth_PassFrom4To1()
-        {
-            // Arrange
-            Coordinate testCoordinate = new(1, 1);
-            Robot testRobot = new(testCoordinate, Direction.WEST);
-            var action = new RobotAction();
+    //    }
+    //    [Fact]
+    //    public void LookRight_FinishLookingNorth_PassFrom4To1()
+    //    {
+    //        // Arrange
+    //        Coordinate testCoordinate = new(1, 1);
+    //        Robot testRobot = new(testCoordinate, Direction.WEST);
+    //        var action = new RobotAction();
 
-            // Act
-            action.LookRight(testRobot);
+    //        // Act
+    //        action.LookRight(testRobot);
 
-            // Assert
-            Assert.Equal(Direction.NORTH, testRobot.Facing);
+    //        // Assert
+    //        Assert.Equal(Direction.NORTH, testRobot.Facing);
 
-        }
+    //    }
 
-        [Fact]
-        public void LookingMultipleTimes_FinishLookingSouth()
-        {
-            // Arrange
-            Coordinate testCoordinate = new(1, 1);
-            Robot testRobot = new(testCoordinate, Direction.NORTH);
-            var action = new RobotAction();
+    //    [Fact]
+    //    public void LookingMultipleTimes_FinishLookingSouth()
+    //    {
+    //        // Arrange
+    //        Coordinate testCoordinate = new(1, 1);
+    //        Robot testRobot = new(testCoordinate, Direction.NORTH);
+    //        var action = new RobotAction();
 
-            // Act
-            action.LookLeft(testRobot);
-            action.LookRight(testRobot);
-            action.LookRight(testRobot);
-            action.LookLeft(testRobot);
-            action.LookLeft(testRobot);
-            action.LookLeft(testRobot);
-            action.LookLeft(testRobot);
-            action.LookRight(testRobot);
+    //        // Act
+    //        action.LookLeft(testRobot);
+    //        action.LookRight(testRobot);
+    //        action.LookRight(testRobot);
+    //        action.LookLeft(testRobot);
+    //        action.LookLeft(testRobot);
+    //        action.LookLeft(testRobot);
+    //        action.LookLeft(testRobot);
+    //        action.LookRight(testRobot);
 
-            // Assert
-            Assert.Equal(Direction.SOUTH, testRobot.Facing);
+    //        // Assert
+    //        Assert.Equal(Direction.SOUTH, testRobot.Facing);
 
-        }
-    }
+    //    }
+    //}
 }
