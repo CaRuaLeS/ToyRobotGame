a99bcbc96857771c127138ae368e5befaa2898c3
Carlos <carua97@gmail.com>
2023-06-10 11:16:14 +0200

    Add placeRobot function to the IRobotActions interface

--- ToyRobotGame/src/Interfaces/IRobotActions.cs
+++ ToyRobotGame/src/Interfaces/IRobotActions.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToyRobotGame.src.Identities;
 
 namespace ToyRobotGame.src.Interfaces
 {
     public interface IRobotActions
     {
+        void PlaceRobot(int row, int column, Direction facing);
         void Move();
         void LookLeft();
         void LookRight();
