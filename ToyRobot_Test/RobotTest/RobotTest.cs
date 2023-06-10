e4d035153c164bc852f99e78f52340ebd8d898a7
Carlos <carua97@gmail.com>
2023-06-11 00:21:09 +0200

    Add PlaceRobot function test

--- ToyRobot_Test/RobotTest/RobotTest.cs
+++ ToyRobot_Test/RobotTest/RobotTest.cs
@@ -21,7 +21,7 @@ namespace ToyRobot_Test.RobotTest
             Robot testRobot = new Robot();
 
             // Act
-            testRobot.PlaceRobot(1, 1, Direction.NORTH);
+            testRobot.PlaceRobot(testRow, testColumn, testFacing);
 
             // Assert
             Assert.Equal(testFacing, testRobot.Facing);
@@ -30,5 +30,48 @@ namespace ToyRobot_Test.RobotTest
 
 
         }
+        [Fact]
+        public void Robot_PlaceRobotFunction_CallTwiceCorrectCoordinates()
+        {
+            // Arrange
+            int testRow = 3;
+            int testColumn = 2;
+            var testFacing = Direction.SOUTH;
+            Robot testRobot = new Robot();
+
+            // Act
+            testRobot.PlaceRobot(1, 1, Direction.NORTH);
+            testRobot.PlaceRobot(testRow, testColumn, testFacing);
+
+            // Assert
+            Assert.Equal(testFacing, testRobot.Facing);
+            Assert.Equal(testRow, testRobot.Position.Row);
+            Assert.Equal(testColumn, testRobot.Position.Column);
+
+
+        }
+        [Fact]
+        public void Robot_PlaceInvalidRobotFunction_FinalShouldBeFirstCoordinates()
+        {
+            // Arrange
+            int testRow = 1;
+            int testColumn = 1;
+            var testFacing = Direction.SOUTH;
+            int testInvalidRow = 8;
+            int testInvalidColumn = 9;
+            Robot testRobot = new Robot();
+
+            // Act
+            testRobot.PlaceRobot(testRow, testColumn, testFacing);
+            testRobot.PlaceRobot(testInvalidRow, testInvalidColumn, testFacing);
+
+            // Assert
+            Assert.Equal(testFacing, testRobot.Facing);
+            Assert.Equal(testRow, testRobot.Position.Row);
+            Assert.Equal(testColumn, testRobot.Position.Column);
+
+
+        }
+        
     }
 }
