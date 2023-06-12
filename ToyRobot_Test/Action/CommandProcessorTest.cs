7a26820873179862884b9f418112da47523c5955
CarlosRuiz <carua97@gmail.com>
2023-06-12 12:22:03 +0200

    Add CommandProcessor test file, PlaceRobot command test added (IN PROGRESS)

---
+++ ToyRobot_Test/Action/CommandProcessorTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToyRobotGame.src.Action;
+using ToyRobotGame.src.Identities;
+using ToyRobotGame.src.Robot;
+using Xunit;
+
+namespace ToyRobot_Test.Action
+{
+    public class CommandProcessorTest
+    {
+        [Fact]
+        public void Command_PlaceRobotAction_PLaceIs23WEST()
+        {
+            // Arrange
+            Robot testRobot = new Robot();
+            CommandProcessor testProcessor = new CommandProcessor(testRobot);
+            string testCommand = "PLACE_ROBOT 2,3,WEST";
+
+            // Act
+            testProcessor.ProcessCommand(testCommand);
+
+            // Assert
+            Assert.Equal(2, testRobot.Position.Row);
+            Assert.Equal(3, testRobot.Position.Column);
+            Assert.Equal(Direction.WEST, testRobot.Facing);
+
+        }
+    }
+}
