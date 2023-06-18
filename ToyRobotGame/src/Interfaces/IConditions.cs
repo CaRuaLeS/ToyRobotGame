41d33df965025551333b810ab7aab980a34fe9f4
Carlos <carua97@gmail.com>
2023-06-18 11:59:44 +0200

    Add conditions interface

---
+++ ToyRobotGame/src/Interfaces/IConditions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToyRobotGame.src.Identities;
+using ToyRobotGame.src.Obstacles;
+using ToyRobotGame.src.Robot;
+
+namespace ToyRobotGame.src.Interfaces
+{
+    public interface IConditions
+    {
+        bool IsValidCoordinate(Coordinate position);
+        bool IsOccupiedCoordinate (Coordinate position, List<Wall> walls, Robot.Robot robot);
+    }
+}
