52be5b2f5aabd1c7398450b114af8efc0afe00f6
Carlos <carua97@gmail.com>
2023-06-21 18:17:46 +0200

    Modified Program.cs. Add Initial text when start game

--- ToyRobotGame/Program.cs
+++ ToyRobotGame/Program.cs
@@ -11,6 +11,10 @@ namespace ToyRobotGame // Note: actual namespace depends on the project name.
             Robot robot = new Robot();
             CommandProcessor commandProcessor = new CommandProcessor(robot);
 
+            Console.WriteLine("WELCOME TO TOYROBOT! |O.O|");
+            // Robot ASCII
+            Console.WriteLine("        _____\n     __/__|__\\__\n    |            |\n    |  0      0  |\n    |     >      |\n    |  \\_____/   |\n    |____________| \n");
+            Console.WriteLine("Inser command here:");
             while (true)
             {
                 string userInput = Console.ReadLine();
