f0493ce541c4a464ca8c229158d2014b9e61b57c
CarlosRuiz <carua97@gmail.com>
2023-06-21 11:21:46 +0200

    Modified Conditions functions, improve them for usage.

--- ToyRobotGame/src/Helpers/Conditions.cs
+++ ToyRobotGame/src/Helpers/Conditions.cs
@@ -1,24 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ToyRobotGame.src.Identities;
+﻿using ToyRobotGame.src.Identities;
 using ToyRobotGame.src.Interfaces;
 using ToyRobotGame.src.Obstacles;
-using ToyRobotGame.src.Robot;
 
 namespace ToyRobotGame.src.Helpers
 {
     public class Conditions: IConditions
     {
         private readonly int XYBoardSize;
-        private readonly List<Wall> walls;
 
-        public Conditions(int boardSize, List<Wall> instanceWalls) 
+        public Conditions(int boardSize) 
         { 
             XYBoardSize = boardSize;
-            walls = instanceWalls;
         }
 
         public bool IsValidCoordinate(Coordinate position)
@@ -29,7 +21,7 @@ namespace ToyRobotGame.src.Helpers
             }
             return false;
         }
-        public bool IsOccupiedCoordinate(Coordinate position, Robot.Robot robot)
+        public bool IsOccupiedCoordinate(Coordinate position, Robot.Robot robot, List<Wall> walls)
         {
             if (robot.Position != null && robot.Position.Row == position.Row && robot.Position.Column == position.Column
                 || walls.Exists(wall => wall.Position.Row == position.Row && wall.Position.Column == position.Column))
