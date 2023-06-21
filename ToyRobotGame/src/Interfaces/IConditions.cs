d05fb507618424b9f3b409078e86b3cd9c091a2a
CarlosRuiz <carua97@gmail.com>
2023-06-21 11:21:01 +0200

    Modified Conditions interface. Add wall to IsOccupiedCoordinate and remove unused usings

--- ToyRobotGame/src/Interfaces/IConditions.cs
+++ ToyRobotGame/src/Interfaces/IConditions.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ToyRobotGame.src.Identities;
+﻿using ToyRobotGame.src.Identities;
 using ToyRobotGame.src.Obstacles;
-using ToyRobotGame.src.Robot;
 
 namespace ToyRobotGame.src.Interfaces
 {
     public interface IConditions
     {
         bool IsValidCoordinate(Coordinate position);
-        bool IsOccupiedCoordinate (Coordinate position, Robot.Robot robot);
+        bool IsOccupiedCoordinate (Coordinate position, Robot.Robot robot, List<Wall> walls);
     }
 }
