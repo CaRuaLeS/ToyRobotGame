27d5bc8e7eed08326f6fb0672b4576dba0e3365e
Carlos <carua97@gmail.com>
2023-06-23 19:13:32 +0200

    Modified Program, Add restart function to the game

--- ToyRobotGame/Program.cs
+++ ToyRobotGame/Program.cs
@@ -1,28 +1,47 @@
-﻿using System;
-using ToyRobotGame.src.Action;
+﻿using ToyRobotGame.src.Action;
 using ToyRobotGame.src.Robot;
 
 namespace ToyRobotGame // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Robot robot = new Robot();
-            CommandProcessor commandProcessor = new CommandProcessor(robot);
+            bool restartGame = false;
+
+            Robot robot = new();
+            CommandProcessor commandProcessor = new(robot);
 
             Console.WriteLine("WELCOME TO TOYROBOT! |O.O|");
             // Robot ASCII
             Console.WriteLine("        _____\n     __/__|__\\__\n    |            |\n    |  0      0  |\n    |     >      |\n    |  \\_____/   |\n    |____________| \n");
-            Console.WriteLine("Inser command here:");
+            Console.WriteLine("Insert command here:");
+            
             while (true)
             {
-                string userInput = Console.ReadLine();
+                if (restartGame)
+                {
+                    robot = new();
+                    commandProcessor = new(robot);
+                    restartGame = false;
+
+                    Console.WriteLine("Game restarted. Insert command here:");
+                }
+
+                string? userInput = Console.ReadLine();
                 if (userInput == "END GAME")
                     break;
+
+                if (userInput == "RESTART")
+                {
+                    Console.Clear();
+                    restartGame = true;
+                    continue;
+                }
                 
                 try 
                 {
+                    // Warning of userInput - NULL is handled inside CommandProcessor
                     commandProcessor.ProcessCommand(userInput);
                 }
                 catch (CustomException ex)
